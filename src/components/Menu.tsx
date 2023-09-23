12fbfb2897f20b7bc65da82a9a5820a1b3446eb5
dokimion24 <leecy3610@gmail.com>
2023-09-23 15:57:37 +0900

    feat: List, Item 컴포넌트 추가

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,8 +2,8 @@ import { createContext, useContext, useState } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 
 type MenuContext = {
-  open: boolean;
-  toggle: (open: boolean) => void;
+  isOpen: boolean;
+  setIsOpen: (open: boolean) => void;
 };
 
 type Props = {
@@ -13,23 +13,45 @@ type Props = {
 const MenuContext = createContext<MenuContext | null>(null);
 
 export function Menu({ children }: Props) {
-  const [open, toggle] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const state = { open, toggle };
+  const state = { isOpen, setIsOpen };
 
   return <MenuContext.Provider value={state}>{children}</MenuContext.Provider>;
 }
 
 function Toggle() {
-  const { open, toggle }: MenuContext = useContext(MenuContext);
+  const { isOpen, setIsOpen } = useContext(MenuContext)!;
 
   return (
     <button
       className='absolute w-8 h-8 top-1 right-1 rounded-full bg-black flex justify-center items-center'
-      onClick={() => toggle(!open)}>
+      onClick={() => setIsOpen(!isOpen)}>
       <AiOutlineMenu color='#fff' />
     </button>
   );
 }
 
+function List({ children }: Props) {
+  const { isOpen } = useContext(MenuContext)!;
+
+  return (
+    <>
+      {isOpen && (
+        <ul className='absolute w-24  top-10 right-4 rounded-sm bg-black flex flex-col justify-center items-center'>
+          {children}
+        </ul>
+      )}
+    </>
+  );
+}
+
+function Item({ children }: Props) {
+  return (
+    <li className='text-white cursor-pointer w-full text-center'>{children}</li>
+  );
+}
+
+Menu.Item = Item;
+Menu.List = List;
 Menu.Toggle = Toggle;
