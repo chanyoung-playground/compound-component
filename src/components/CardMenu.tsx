71f47043592dd94559bd296d96fc7b45bb5b3640
dokimion24 <leecy3610@gmail.com>
2023-09-23 15:58:44 +0900

    feat: compound pattern 적용

--- src/components/CardMenu.tsx
+++ src/components/CardMenu.tsx
@@ -5,6 +5,10 @@ export default function CardMenu() {
   return (
     <Menu>
       <Menu.Toggle />
+      <Menu.List>
+        <Menu.Item>Edit</Menu.Item>
+        <Menu.Item>Delete</Menu.Item>
+      </Menu.List>
     </Menu>
   );
 }
